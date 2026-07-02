98261968edbd0126b6f0b99293d5848aaa8fc3c8
EDUARDA FERRAZZA STEIN <eduarda_stein@estudante.sesisenai.org.br>
2026-07-02 19:25:16 -0300

    feat: estrutura inicial da MainActivity

--- app/src/main/java/com/example/prova_mobile/MainActivity.kt
+++ app/src/main/java/com/example/prova_mobile/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.prova_mobile
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edtLinha: EditText
+    private lateinit var edtOperador: EditText
+    private lateinit var txtContador: TextView
+    private lateinit var btnIniciar: Button
+    private lateinit var btnRegistrar: Button
+    private lateinit var btnFinalizar: Button
+
+    private var contador = 0
+    private var inicio = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        edtLinha = findViewById(R.id.edtLinha)
+        edtOperador = findViewById(R.id.edtOperador)
+        txtContador = findViewById(R.id.txtContador)
+        btnIniciar = findViewById(R.id.btnIniciar)
+        btnRegistrar = findViewById(R.id.btnRegistrar)
+        btnFinalizar = findViewById(R.id.btnFinalizar)
+
+        btnRegistrar.isEnabled = false
     }
 }
