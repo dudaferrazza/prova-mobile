c47c272b2d54e2a68a4c9e57853a88fc710c66b6
EDUARDA FERRAZZA STEIN <eduarda_stein@estudante.sesisenai.org.br>
2026-07-02 20:23:28 -0300

    feat: cria estrutura inicial da RelatorioActivity

--- app/src/main/java/com/example/prova_mobile/RelatorioActivity.kt
+++ app/src/main/java/com/example/prova_mobile/RelatorioActivity.kt
@@ -1,4 +1,18 @@
 package com.example.prova_mobile
 
-class RelatorioActivity {
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Locale
+
+class RelatorioActivity : AppCompatActivity() {
+
+    private lateinit var txtResultado: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relatorio)
+
+        txtResultado = findViewById(R.id.txtResultado)
+    }
 }
